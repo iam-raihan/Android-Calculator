5aeeb8b828f00ead711d344d4d79043dfba13ca2
Raihan Al-Mamun <iam.raihan96@gmail.com>
2017-08-27 16:29:14 +0600

    eval added

--- src/com/raihan/calculator/AndroidCalculatorActivity.java
+++ src/com/raihan/calculator/AndroidCalculatorActivity.java
@@ -9,110 +9,170 @@ import android.widget.EditText;
 public class AndroidCalculatorActivity extends Activity {
 	
 	private EditText textBox;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        textBox = (EditText)findViewById(R.id.editText1);
-    }
-    
-    public void numBtn (View btn) {
-    	int cursorPosStart = textBox.getSelectionStart();
-    	CharSequence textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, ((Button) btn).getText().toString());
-		setTextBoxText(textBoxText, cursorPosStart);
-    }
-    
-    public void signBtn (View btn) {
-    	int cursorPosStart = textBox.getSelectionStart();
-    	CharSequence textBoxText = textBox.getText();
-    	if ( cursorPosStart == 0) {
-    		return;
-    	}
-    	if (textBoxText.charAt(cursorPosStart - 1) == '+'
-				|| textBoxText.charAt(cursorPosStart - 1) == '-'
-				|| textBoxText.charAt(cursorPosStart - 1) == '*'
-				|| textBoxText.charAt(cursorPosStart - 1) == '/') {
-			String text = textBoxText.toString();
-			text = text.substring(0, cursorPosStart - 1)+((Button) btn).getText().toString();
-			if (cursorPosStart != textBox.getText().length()) {
-				 text += textBoxText.toString().substring(cursorPosStart);
+	private int curPos;
+	private String textBoxText;
+	private String inputText;
+
+	private void initialize (View btn) {
+		curPos = textBox.getSelectionStart();
+		textBoxText = textBox.getText().toString();
+		if (textBoxText.contains("Invalid Expression") 
+				|| textBoxText.contains("NaN")
+				|| textBoxText.contains("Infinity")
+				|| textBoxText.contains("E")) {
+			textBox.setText("");
+			textBoxText = "";
+			curPos = 0;
+		}
+		inputText = ((Button) btn).getText().toString();
+	}
+	
+	private boolean isArithmetic (char ch) {
+		return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+	}
+	
+	private boolean isSign (char ch) {
+		return isArithmetic(ch) || ch == '√';
+	}
+	
+	private boolean isDigit (char ch) {
+		return ch >= '0' && ch <= '9'; // or return Character.isDigit(char);
+	}
+
+	private boolean isUnary (char ch) {
+		return ch == '-'
+				? textBoxText.length() == 0 
+					|| isSign(textBoxText.charAt(curPos - 1))
+				: false;
+	}
+	
+	private boolean notUsed (char ch, int pos) {
+		char[] text = textBoxText.toCharArray();
+		while (pos != 0) {
+			if (!isDigit(text[pos - 1])) {
+				if (isArithmetic(text[pos - 1])) return true;
+				if (text[pos - 1] == ch) return false;
 			}
-			setTextBoxText(text, cursorPosStart - 1);
+			pos--;
+		}    	
+		return true;
+	}
+
+	private void setText (String text, int pos) {
+		textBox.setText("");
+		textBox.append(text);
+		textBox.setSelection(pos);
+	}
+	
+	private void insertText (String newText, int newPosFromCur) {
+		textBoxText = new StringBuilder(textBox.getText()).insert(curPos, newText).toString();
+		setText(textBoxText, curPos + newPosFromCur);
+	}
+	
+	private void replaceText (String newText, int newPosFromCur) {
+		String text = textBoxText;
+		text = text.substring(0, curPos - 1) + newText;
+		if (curPos != textBox.getText().length())
+			 text += textBoxText.substring(curPos);
+		setText(text, curPos + newPosFromCur);
+	}
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    
+	    textBox = (EditText)findViewById(R.id.editText1);
+	}
+	
+	public void equalBtn (View btn) {
+		Evaluate evalObj = new Evaluate(textBox.getText().toString());
+		double result = evalObj.eval();
+		String resultText;
+		if (evalObj.invalid)
+			return; //resultText = "Invalid Expression";
+		else if (result == Math.rint(result)) {
+			resultText = new Integer((int)result).toString();
+			if (resultText.equals(String.valueOf(Integer.MAX_VALUE)))
+				resultText = new Double(result).toString();
+		}
+		else
+			resultText = new Double(result).toString();
+		setText(resultText, resultText.length());
+	}
+	
+	public void numBtn (View btn) {
+		initialize(btn);
+
+		insertText(inputText, 1);
+	}
+	
+	public void signBtn (View btn) {
+		initialize(btn);
+		
+		if (curPos == 0) {
+			if (isUnary(inputText.charAt(0)))
+				insertText(inputText, 1);
+		}
+		else if (isDigit(textBoxText.charAt(curPos - 1))) {
+			insertText(inputText, 1);
+		}
+		else if (textBoxText.charAt(curPos - 1) == '.') {
+			insertText("0" + inputText, 2);
+		}
+		else if (curPos < textBox.getText().length() && textBoxText.charAt(curPos) == '.') {
+			insertText(inputText + "0", 2);
+		}
+		else if (isSign(textBoxText.charAt(curPos - 1))) {
+			if (isUnary(inputText.charAt(0)))
+				insertText(inputText, 1);
+			else if (curPos > 1 && isDigit(textBoxText.charAt(curPos - 2)))
+				replaceText(inputText, 0);
+		}
+	}
+
+	public void rootBtn (View btn) {
+		initialize(btn);
+		int pos = curPos;
+		if (notUsed('√', curPos)) {
+			char[] text = textBoxText.toCharArray();
+			while (curPos != 0) {
+				if (! isDigit(text[curPos - 1]) && text[curPos - 1] != '.') {
+					break;
+				}
+				curPos--;
+			}
+			insertText(inputText, pos - curPos + 1);
+		}
+	}
+	
+	public void dotBtn (View btn) {
+		initialize(btn);
+		
+		if (! notUsed('.', curPos)) {
 			return;
 		}
-    	if (cursorPosStart == textBox.getText().length()) {
-    		if (Character.isDigit(textBoxText.charAt(cursorPosStart - 1))) {
-        		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, ((Button) btn).getText().toString());
-            	setTextBoxText(textBoxText, cursorPosStart);
-        	}
-    		else if (textBoxText.charAt(cursorPosStart - 1) == '.') {
-        		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, "0" + ((Button) btn).getText().toString());
-            	setTextBoxText(textBoxText, cursorPosStart + 1);
-        	}
-    	}
-    	else if (cursorPosStart < textBox.getText().length()) {
-    		if (Character.isDigit(textBoxText.charAt(cursorPosStart - 1)) && Character.isDigit(textBoxText.charAt(cursorPosStart))) {
-        		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, ((Button) btn).getText().toString());
-            	setTextBoxText(textBoxText, cursorPosStart);
-        	}
-    		else if (textBoxText.charAt(cursorPosStart - 1) == '.') {
-        		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, "0" + ((Button) btn).getText().toString());
-            	setTextBoxText(textBoxText, cursorPosStart + 1);
-        	}
-    		else if (textBoxText.charAt(cursorPosStart) == '.') {
-        		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, ((Button) btn).getText().toString() + "0");
-            	setTextBoxText(textBoxText, cursorPosStart + 1);
-        	}
-    	}
-    	
-    }
-    
-    public void dotBtn (View btn) {
-    	int cursorPosStart = textBox.getSelectionStart();
-    	CharSequence textBoxText = textBox.getText();
-    	if ( cursorPosStart == 0 
-    			|| (!Character.isDigit(textBoxText.charAt(cursorPosStart - 1)) 
-    					&& textBoxText.charAt(cursorPosStart - 1) != '.')) {
-    		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, "0" + ((Button) btn).getText().toString());
-        	setTextBoxText(textBoxText, cursorPosStart + 1);
-    	}
-    	if ( cursorPosStart != 0 && Character.isDigit(textBoxText.charAt(cursorPosStart - 1)) && !dotUsed(cursorPosStart)) {
-    		textBoxText = new StringBuilder(textBox.getText()).insert(cursorPosStart, ((Button) btn).getText().toString());
-        	setTextBoxText(textBoxText, cursorPosStart);
-    	}
-    }
-    
-    private boolean dotUsed (int pos) {
-    	char[] text = textBox.getText().toString().toCharArray();
-    	if (text.length == 0) return false;
-    	boolean flag = false;
-    	int curPos = pos;
-    	while (curPos != 0) {
-    		if (!Character.isDigit(text[curPos - 1])) {
-    			flag = text[curPos - 1] == '.';
-    			break;
-    		}
-    		curPos--;
-    	}
-    	if (! flag) {
-    		curPos = pos;
-    		while (curPos < text.length) {
-        		if (!Character.isDigit(text[curPos - 1])) {
-        			flag = text[curPos - 1] == '.';
-        			break;
-        		}
-        		curPos++;
-        	}
-    	}
-    	
-    	return flag;
-    }
-    
-    private void setTextBoxText(CharSequence text, int pos) {
-    	textBox.setText("");
-		textBox.append(text);
-		textBox.setSelection(pos + 1);
-    }
+		if ( curPos == 0 || !isDigit(textBoxText.charAt(curPos - 1))) {
+			insertText("0" + inputText, 2);
+		}
+		else if ( isDigit(textBoxText.charAt(curPos - 1))) {
+			insertText(inputText, 1);
+		}
+	}
+	
+	public void clearBtn (View btn) {
+		textBox.setText("");
+	}
+	
+	public void backBtn (View btn) {
+		initialize(btn);
+		
+	 	if ( curPos == 0) {
+	 		return;
+	 	}
+	 	else {
+	 		replaceText("", -1);
+	 	}
+	}
 }
---
+++ src/com/raihan/calculator/Evaluate.java
@@ -0,0 +1,93 @@
+package com.raihan.calculator;
+
+public class Evaluate {
+	private int pos = -1;
+	private int ch;							//pointer
+	private String expression;
+	public boolean invalid = false;
+	
+	public Evaluate(String str) {
+		this.expression = str;
+	}
+
+	private void nextChar() {				//move pointer to next
+        ch = (++pos < expression.length()) 
+        		? expression.charAt(pos) 
+        				: -1;
+    }
+	
+	private boolean isDigit() {
+		return ch >= '0' && ch <= '9'; 		// or return Character.isDigit(char);
+	}
+	
+	private double getNumber(int startPos) {
+		while (isDigit() || ch == '.')
+        	nextChar();
+        return Double.parseDouble(expression.substring(startPos, this.pos));
+	}
+	
+	private boolean isSign(int sign) {		//check if current pointer is a sign
+        if (ch == sign) {
+            nextChar();
+            return true;
+        }
+        return false;
+    }
+	
+	public double eval() {		
+		try {
+			nextChar();
+	        double x = getExpression();
+	        if (pos < expression.length()) 
+	        	throw new RuntimeException();
+	        return x;
+		}
+		catch (RuntimeException ex){
+			invalid = true;
+			return 0;
+		}
+	}
+
+	private double getExpression() {		//solves 3+3 or 3-3
+        double x = getTerm();
+        for (;;) {
+            if (isSign('+')) 
+            	x += getTerm();
+            else if (isSign('-'))
+            	x -= getTerm();
+            else
+            	return x;
+        }
+    }
+
+	private double getTerm() {				//solves: 3*3 or 9/3
+        double x = getFactor();
+        for (;;) {
+            if (isSign('*')) 
+            	x *= getFactor();
+            else if (isSign('/')) 
+            	x /= getFactor();
+            else
+            	return x;
+        }
+    }
+
+	private double getFactor() {			//solves: √9.5 or 3.5^3 or -3 or 35.8 or 10  
+        if (isSign('-')) 
+        	return -getFactor(); 			// unary minus
+        if (isSign('√'))
+        	return Math.sqrt(getFactor());
+        
+        double num;
+        int startPos = this.pos;
+        if (isDigit() || ch == '.')
+            num = getNumber(startPos);
+        else
+            throw new RuntimeException();
+        
+        if (isSign('^')) 
+        	num = Math.pow(num, getFactor());
+
+        return num;
+    }	
+}
